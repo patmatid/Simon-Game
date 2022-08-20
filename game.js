5dd52f3f824f7ba55fc16a334811a7c7b58956a2
patmatid <patmatid@hotmail.com>
2022-08-20 15:17:47 +0200

    Fisished

--- game.js
+++ game.js
@@ -4,10 +4,20 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
 var userClickedPattern = [];
-var gameHasStarted = false;
-var iLevel = 0;
+var isGameRunning = false;
+var level = 0;
+var isGameOver = false;
+var randomChosenColour;
+
+/* Declareer variabele zonder te initialiseren.Zelfs initialisatie naar "" is niet goed want dan wordt deze al
+op de array userClickedPattern gepushed zonder dat er een event plaats vond in de push hieronder
+Vergeet niet dat de eventhandler een callback routine is die enkel gecalled wordt als het event plaats gevonden
+heeft. Vandaar beter eventhanhler in functie plaatsen en returnwaarde klaar
+*/
+var userChosenColour; // Dus globaal gedefinieerd
 
 function nextSequence() {
+  level++;
   return Math.floor(Math.random() * 4);
 }
 
@@ -16,12 +26,13 @@ function doFlashing() {
   $("#" + randomChosenColour).fadeIn(tInterval / 2);
 }
 
-var randomChosenColour = buttonColours[nextSequence()];
-gamePattern.push(randomChosenColour);
+function lightupNewColour(color) {
+  $("#" + color).fadeOut(100).fadeIn(100);
+};
 
 var tInterval = 1000;
 
-var timerId = setInterval(doFlashing, tInterval); //globale variabele
+//var timerId = setInterval(doFlashing, tInterval); //globale variabele
 
 function playSound(name) {
   return new Audio("sounds/" + name + ".mp3");
@@ -38,14 +49,15 @@ aangemaakt=> moet CB zijn!!!*/
 
 //Andere mogelijkheid
 //Begin
-var toggleAnimation = function() {
+var toggleStartStopGame = function() {
   //  alert("" + event + "/" + timerId);
-  if (gameHasStarted) {
-    clearInterval(timerId); //Stop game; stop flashing
+  if (isGameRunning) {
+    window.location.reload(true); //Stop game; stop flashing
   } else {
-    $("h1#level-title").text("Level " + iLevel);
+    newStep();
+    $("h1#level-title").text("Level " + level);
   }
-  gameHasStarted = !gameHasStarted;
+  isGameRunning = !isGameRunning;
 }
 
 function animatePress(currentColour) { //opdracht is dat kleur wordt meegegeven, NIET event!!!
@@ -71,27 +83,50 @@ setTimeout this in  $(this) niet meer herkend wordt
 bind is noodzakelijk in functiecall hieronder.
 cfr uitleg van bind(thisArg, arg1,...,argN) in MDN== mozilla web pages
 In bind(event, timerId) hieronder stelt event "this" voor die aan de
-functie doorgegeven wordt, timerId wordt vanuit game.js doorgegeven. Merk op dat uit de defintie van toggleAnimation blijkt dat
+functie doorgegeven wordt, timerId wordt vanuit game.js doorgegeven. Merk op dat uit de defintie van toggleStartStopGame blijkt dat
 event en timerId geen formele parameters zijn: er zijn er geen gedefinieerd voor de functie hetgeen dus betekent dat event en timerId
 ofwel als  globale variabelen overgenomen worden ofwel via een andere binding die dan gegeven moet zijn.
 Vandaar dus de noodzaak van ".bind" om het event door te geven dat via de context van $(document) opgepikt wordt
-Als we bind niet genruiken wordt de functie toggleAnimation synchroon gecalled en is het dus geen callback gedefinieerd en is er dus ook
+Als we bind niet genruiken wordt de functie toggleStartStopGame synchroon gecalled en is het dus geen callback gedefinieerd en is er dus ook
 geen eventhandler gedefinieerd. Vandaar dat pagina niet reageert op "keydown"
 Volgens doc van jquery best zo selectief mogelijk zijn met de $() variabele om query te versnellen
 */
-$(document).on("keydown", toggleAnimation.bind(event, timerId));
+$(document).on("keydown", toggleStartStopGame.bind(event));
 //Einde
 
-/* Declareer variabele zonder te initialiseren.Zelfs initialisatie naar "" is niet goed want dan wordt deze al
-op de array userClickedPattern gepushed zonder dat er een event plaats vond in de push hieronder
-Vergeet niet dat de eventhandler een callback routine is die enkel gecalled wordt als het event plaats gevonden
-heeft. Vandaar beter eventhanhler in functie plaatsen en returnwaarde klaar
-*/
-var userChosenColour; // Dus globaal gedefinieerd
+function newStep() {
+  randomChosenColour = buttonColours[nextSequence()];
+  lightupNewColour(randomChosenColour);
+  gamePattern.push(randomChosenColour);
+};
 
-$("div.btn").on("click", function() {
-  userChosenColour = $(this).attr("id");
-  userClickedPattern.push(userChosenColour); //pushen moet in eventhandler gebeuren want er worden geen statements meer uitgevoerd in game.js bij klikken van kleur indien niet gelooped wordt
+function treatButtonClick(jqElem) {
+  userChosenColour = jqElem.target.id;
+  userClickedPattern.push(userChosenColour);
   animatePress(userChosenColour);
   playSound(userChosenColour).play();
-});
+  if (userClickedPattern.length == gamePattern.length) {
+    for (var idx = 0; idx < gamePattern.length; idx++) {
+      isGameOver = (gamePattern[idx] !== userClickedPattern[idx]);
+      if (isGameOver) {
+        $("h1#level-title").text("Error: game over");
+        $("h1").addClass("game-over");
+        playSound("wrong").play();
+        $("div.btn").off();; //switch off all btn eventhandlers
+        break; //break out of loop
+      }
+    }
+    if (!isGameOver) {
+      newStep();
+      $("h1#level-title").text("Level " + level);
+      userClickedPattern = [];
+    }
+  }
+}
+
+$("div.btn").on("click", treatButtonClick.bind($(this))); //eventhandler luistert zolang je hem niet afzet
+/*
+Dus in dit programma zijn 2 evenhandlers aan het werk:
+1/ treatButtonClick om een druk op een button te behandelen
+2/ toggleStartStopGame om het spel te starten en te stoppen
+*/
